3ce9f64ba502ccf7b4b6c7642e9ec7c7ea7423d2
Richard Robert Reitz <richard-robert.reitz@t-systems.com>
2022-08-17 21:03:44 +0200

    Removed test gbp upload

--- tests/sign-detach-verify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-signed="test.signed"
-sig="test.sig"
-
-"$sbsign" --cert "$cert" --key "$key" --output "$signed" "$image"
-"$sbattach" --detach "$sig" "$signed"
-"$sbverify" --cert "$cert" --detached $sig "$image"
